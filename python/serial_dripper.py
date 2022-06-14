eff82a1d09e2ef93ad4eb84bb9b5cd545207de98
Kurosawa Makoto <piper9638@gmail.com>
2022-06-14 17:12:35 +0900

    tk

--- python/serial_dripper.py
+++ python/serial_dripper.py
@@ -5,7 +5,8 @@
 """
 import serial
 import tkinter
-import tkinter.filedialog
+from tkinter import filedialog
+from tkinter import ttk
 import threading
 
 
@@ -26,8 +27,23 @@ def main():
     root.title(u'serial dripper')
     root.geometry("400x300")
 
-    snp = serial.Serial(port = 'COM6',  baudrate = 115200, timeout = 0)
-    sno = serial.Serial(port = 'COM8',  baudrate =   9600, timeout = 0)
+    snpFlileName = ttk.Label( root, text = 'file', padding = (5, 10))
+    snpFlileName.grid(row = 0, column = 0)
+
+    snpBtn = ttk.Button( root, text = 'snoop', command = lambda: root.quit())
+    snpBtn.grid(row = 0, column = 1)
+
+    comName = ttk.Label( root, text = 'com', padding = (5, 10))
+    comName.grid(row = 1, column = 0)
+
+    snoBtn = ttk.Button( root, text = 'serialNo', command = lambda: root.quit())
+    snoBtn.grid(row = 1, column = 1)
+
+    quitBtn = ttk.Button( root, text = 'quit', command = lambda: root.quit())
+    quitBtn.grid(row = 2, column = 0)
+
+    snp = serial.Serial(port = 'COM10',  baudrate = 115200, timeout = 0)
+    sno = serial.Serial(port = 'COM12',  baudrate =   9600, timeout = 0)
 
     run = False
     period = threading.Thread(target = periodic)
